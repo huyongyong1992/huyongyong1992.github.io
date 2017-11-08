73ef403eefb6e9e0c7fbd98c8bc886aa8910c71b
huyongyong <1140175373@qq.com>
2017-11-08 17:50:34 +0800

    haha

--- src/service/getData.js
+++
@@ -1,9 +0,0 @@
-import { getInfo } from '../config/axios';
-import { appGetInfo } from '../config/appAxios';
-import { url,wxapiUrl } from '../config/env'
-
-/**
- * lalala
- * @param { }
- */
-export const getMovieList= (params) => getInfo('https://github.com/huyongyong1992/python_study/blob/master/movie.json',{},'get');
